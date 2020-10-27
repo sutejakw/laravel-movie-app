3ca2cd5b08605db6fca94b998cb41f86cb1fce6b
sutejakw <sutejakawinanga29@gmail.com>
2020-10-27 15:57:30 +0800

    create index and detail page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            witdh: {
+                "96": "24rem"
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
