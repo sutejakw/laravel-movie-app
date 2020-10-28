66428f73629a6c764f559c964a5ca9ff4dde95df
sutejakw <sutejakawinanga29@gmail.com>
2020-10-28 14:42:31 +0800

    Integration with tmdb API, testing API

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -7,11 +7,6 @@ use Illuminate\Support\Facades\Http;
 
 class MoviesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
@@ -25,76 +20,58 @@ class MoviesController extends Controller
         $genres = collect($genresArray)->mapWithKeys(function ($genre) {
             return [$genre['id'] => $genre['name']];
         });
-    
 
-        dump($popularMovies);
+        $nowPlayingMovies =  Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/now_playing')
+            ->json()['results'];
+    
         return view('index', [
             'popularMovies' => $popularMovies,
-            'genres' => $genres
+            'genres' => $genres,
+            'nowPlayingMovies' => $nowPlayingMovies
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/'.$id.'?append_to_response=credits,videos,images')
+            ->json();
+
+        $genresArray = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
+        
+        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+
+        return view('show', [
+            'movie' => $movie,
+            'genres' => $genres
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,19 +3,23 @@
 @section('content')
   <div class="movie-info border-b border-gray-800">
     <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
-      <img src="/img/parasite.jpg" alt="Actor" class="w-screen md:w-96">
+      <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$movie['poster_path'] }}" alt="Actor" class="w-auto md:w-96">
       <div class="md:ml-24">
-        <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
+        <h2 class="text-4xl font-semibold">{{ $movie['title'] }}</h2>
         <div class="flex flex-wrap items-center text-gray-400 text-sm">
           <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
-          <span class="ml-1">85%</span>
+          <span class="ml-1">{{ $movie['vote_average'] * 10 . '%' }}</span>
           <span class="mx-2">|</span>
-          <span>Feb 20, 2020</span>
+          <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y') }}</span>
           <span class="mx-2">|</span>
-          <span>Action, Thriller, Drama</span>
+          <span>
+            @foreach ($movie['genres'] as $genre)
+              {{ $genre['name'] }}@if (!$loop->last), @endif
+            @endforeach
+          </span>
         </div>
         <p class="text-gray-300 mt-8">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam commodi laborum architecto magnam quia excepturi culpa similique, quibusdam, unde eligendi non sunt. Saepe ipsa magni voluptate mollitia quasi, quibusdam optio libero dolorem sequi ab nulla reiciendis? Saepe, doloremque, deserunt blanditiis, obcaecati dolor enim animi totam delectus similique distinctio debitis dolore.
+          {{ $movie['overview'] }}
         </p>
 
         <div class="mt-12">
@@ -23,23 +27,25 @@
             Featured Cast
           </h4>
           <div class="mt-4 flex">
-            <div>
-              <div>Bong Joon-ho</div>
-              <div class="text-sm text-gray-400">Screenplay, Director, Story</div>
-            </div>
-            <div class="ml-8">
-              <div>Han Ji-won</div>
-              <div class="text-sm text-gray-400">Screenplay</div>
-            </div>
+            @foreach ($movie['credits']['crew'] as $crew)
+              @if ($loop->index < 2)
+                <div class="mr-8">
+                  <div>{{ $crew['name'] }}</div>
+                  <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
+                </div>
+              @endif
+            @endforeach
           </div>
         </div>
 
-        <div class="mt-12">
-          <button class="flex items-center bg-orange-400 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-            <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
-            <span class="ml-2">Play Trailer</span>
-          </button>
-        </div>
+        @if (count($movie['videos']['results']) > 0)
+          <div class="mt-12">
+            <a href="https://youtube.com/watch?v={{ $movie['videos']['results'][0]['key'] }}" target="_blank" class="inline-flex items-center bg-orange-400 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+              <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+              <span class="ml-2">Play Trailer</span>
+            </a>
+          </div>
+        @endif
       </div>
     </div>
 
@@ -49,61 +55,23 @@
     <div class="container mx-auto px-4 py-16">
       <h2 class="text-4xl font-semibold">Cast</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-5 gap-8">
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/actor1.jpg" alt="Actor" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-          <div class="mt-2">
-            <a href="#" class="text-lg mt-2 hover:text-gray-300">Real Name</a>
-            <div class="text-gray-400 text-sm">
-              Jhon Smith
+        @foreach ($movie['credits']['cast'] as $cast)
+          @if ($loop->index < 5)
+            <div class="mt-8">
+              <a href="#">
+                
+                <img src="{{ $cast['profile_path'] ? 'https://image.tmdb.org/t/p/w300/'.$cast['profile_path'] : ($cast['gender'] == 1 ? 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-36-user-female-grey-d9222f16ec16a33ed5e2c9bbdca07a4c48db14008bbebbabced8f8ed1fa2ad59.svg' : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg') }}" alt="Cast" class="w-screen hover:opacity-75 transition ease-in-out duration-150">
+                
+              </a>
+              <div class="mt-2">
+                <a href="#" class="text-lg mt-2 hover:text-gray-300">{{ $cast['name'] }}</a>
+                <div class="text-gray-400 text-sm">
+                  {{ $cast['character'] }}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/actor2.jpg" alt="Actor" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-          <div class="mt-2">
-            <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
-            <div class="text-gray-400 text-sm">
-              Jhon Smith
-            </div>
-          </div>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/actor3.jpg" alt="Actor" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-          <div class="mt-2">
-            <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
-            <div class="text-gray-400 text-sm">
-              Jhon Smith
-            </div>
-          </div>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/actor4.jpg" alt="Actor" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-          <div class="mt-2">
-            <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
-            <div class="text-gray-400 text-sm">
-              Jhon Smith
-            </div>
-          </div>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/actor5.jpg" alt="Actor" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-          <div class="mt-2">
-            <a href="#" class="text-lg mt-2 hover:text-gray-300">Parasite</a>
-            <div class="text-gray-400 text-sm">
-              Jhon Smith
-            </div>
-          </div>
-        </div>
+          @endif
+        @endforeach
       </div>
     </div>
   </div> {{-- end cast info --}}
@@ -112,36 +80,13 @@
     <div class="container mx-auto px-4 py-16">
       <h2 class="text-4xl font-semibold">Images</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-3 gap-8">
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image1.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image2.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image3.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image4.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image5.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
-        <div class="mt-8">
-          <a href="#">
-            <img src="/img/image6.jpg" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
-          </a>
-        </div>
+        @foreach ($movie['images']['backdrops'] as $image)
+          <div class="mt-8">
+            <a href="#">
+              <img src="{{ 'https://image.tmdb.org/t/p/w500/'.$image['file_path'] }}" alt="Image" class="hover:opacity-75 transition ease-in-out duration-150">
+            </a>
+          </div>
+        @endforeach
       </div>
     </div>
   </div> {{-- end movie image --}}
